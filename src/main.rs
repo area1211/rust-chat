8f4f0582fb176548e8917dee93d691d006d34aa6
junginho <area1211@naver.com>
2018-10-13 16:59:44 +0900

    socket create

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate mio;
+
+use std::net::SocketAddr;
+use mio::tcp::*;
+
+
+
+struct WebSocketServer;
+
+impl Handler for WebSocketServer{
+  type Timeout = usize;
+  type Message = ();
+}
+
 fn main() {
     println!("Hello, world!");
+    let address = "0.0.0.0:10000".parse::<SocketAddr>().unwrap();
+    let server_socket = TcpListener::bind(&address).unwrap();
+    
+    let mut event_loop = EventLoop::new().unwrap();
+    // 토큰은 소켓의 유일한 식별자이다.
+    event_loop.register(&server_socket, Token(0),
+                        EventSet::readable(), PollOpt::edge()).unwrap();
+
+
+
+    // 핸들러 구조체의 새로운 인스턴스를 만든다.
+    let mut handler = WebSocketServer;
+    // 이벤트 루프에 변할 수 있는 참조를 넘긴다.
+    event_loop.run(&mut handler).unwrap();
 }
